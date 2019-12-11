9123c2b5651f1e19d7a2ba3491d00d68e24c65a2
htljoyj <htljoyj@gmail.com>
2019-12-11 09:18:36 +0100

    added LevelScreen.ts

--- src/LevelScreen.ts
+++ src/LevelScreen.ts
@@ -1,3 +1,36 @@
 class LevelScreen {
+    private readonly canvas: HTMLCanvasElement;
+    private readonly ctx: CanvasRenderingContext2D;
 
+    public constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+    }
+
+    public draw() {
+        
+    }
+
+    /**
+     * Writes text to the canvas
+     * @param {string} text - Text to write
+     * @param {number} fontSize - Font size in pixels
+     * @param {number} xCoordinate - Horizontal coordinate in pixels
+     * @param {number} yCoordinate - Vertical coordinate in pixels
+     * @param {string} alignment - Where to align the text
+     * @param {string} color - The color of the text
+     */
+    public writeTextToCanvas(
+        text: string,
+        fontSize: number = 20,
+        xCoordinate: number,
+        yCoordinate: number,
+        alignment: CanvasTextAlign = "center",
+        color: string = "white",
+    ) {
+        this.ctx.font = `${fontSize}px Minecraft`;
+        this.ctx.fillStyle = color;
+        this.ctx.textAlign = alignment;
+        this.ctx.fillText(text, xCoordinate, yCoordinate);
+    }
 }
