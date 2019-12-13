3b708a515861ad818074846689486de4957e69fb
htljoyj <46250336+htljoyj@users.noreply.github.com>
2019-12-13 10:01:16 +0100

    Update Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -6,7 +6,7 @@ class Game {
     private readonly canvas: HTMLCanvasElement;
     private readonly ctx: CanvasRenderingContext2D;
 
-   
+    private lives: number;
 
     private currentScreen: any;
     private keyboardListener: KeyboardListener;
@@ -22,9 +22,10 @@ class Game {
         // Set the context of the canvas
         this.ctx = this.canvas.getContext("2d");
 
+        this.lives = 3;
 
         this.keyboardListener = new KeyboardListener();
-        this.currentScreen = new LevelScreen(this.canvas, this.ctx);
+        this.currentScreen = new TitleScreen(this.canvas, this.ctx);
 
         this.loop();
     }
@@ -46,7 +47,11 @@ class Game {
     }
 
     private switchScreen() {
-
+        if(this.currentScreen instanceof TitleScreen && this.keyboardListener.isKeyDown(KeyboardListener.KEY_SPACE))
+        {
+            this.currentScreen = new LevelScreen(this.canvas, this.ctx);
+        }
+        // this.currentScreen = new TitleScreen(this.canvas, this.ctx);
     }
 
     /**
